8f95d58c05f4b38600555c04898c5d2a3ae722b2
anouk <a.m.vanderlee@students.uu.nl>
2019-05-23 16:31:15 +0200

    extra update

--- original-repast-model/src/loadPoles/Route.java
+++ original-repast-model/src/loadPoles/Route.java
@@ -1,39 +1,67 @@
 package loadPoles;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import repast.simphony.context.Context;
 import repast.simphony.space.grid.Grid;
 
 //import java.util.List; 
 public class Route {
 
-	//0.2* ;
-	//type: work/hustle/trip/visit
-	public Route(Context<Object> context, Grid<Object> dwellingsGrid){
-		//this.context = context;
-		//this.dwellingsGrid = dwellingsGrid;
-		//number them.
-		//this.name = String.valueOf(context.getObjects(Dwelling.class).size());
+	List <double[]> allArrays;
+	HashMap <String, double[]> legenda;
+
+	
+	double[] bikeArray = {0.1, 2.0};
+	double[] ebikeArray = {6.0, 4.3}; 
+	double[] carArray = {6.0, 4.3}; 
+	double[] evArray = {6.0, 4.3}; 
+	double[] motorArray = {6.0, 4.3}; 
+	double[] ovArray = {6.0, 4.3};
+	double[] walkArray = {6.0, 4.3};
+	
+
+	// 0.2* ;
+	// type: work/hustle/trip/visit
+	public Route(int startnode, int endnode){
+		allArrays = new ArrayList<double[]>();
+		legenda = new HashMap<String, double[]>();
+		//
+		legenda.put("bicycle", bikeArray);
+		legenda.put("e-bike", ebikeArray);
+		legenda.put("car", carArray);
+		legenda.put("EV", evArray);
+		legenda.put("walk", walkArray);
+		legenda.put("motor", motorArray);
+		legenda.put("ov", ovArray);
 	}
+
 	
-	
-	//given: beginlocatie/eindlocatie/zones?
-	
-	
-	//per vehicle	
-		//afstand
-	//new york distance
-		//time
-		//zones
-	
-	
-		//cost
-		//comfort
-		//energy 
-		//CO2
-		
-	
-	
-	//returns route list
-	
-	
+public void getValues ()
+{
+	//determine the comfort of all vehicles
+	evArray[1] = ;
+
+
+
+	// afstand
+	// new york distance
+	// time
+	// zones
+
+	// cost
+	// comfort
+	// energy
+	// CO2
+
+	// saves route list
+	// route list = double array
+}
+
+
+
 }
